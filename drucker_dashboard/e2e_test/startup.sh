946a8f32253af0c47188f37d9b2364d519cd62ee
Hattori Keigo <keigoht@gmail.com>
2019-01-09 09:11:46 +0900

    Update minikube startup script

--- drucker_dashboard/e2e_test/startup.sh
+++ drucker_dashboard/e2e_test/startup.sh
@@ -11,28 +11,27 @@ configure_cluster () {
     echo "Start to configure $cluster_name"
     minikube start -p $cluster_name --kubernetes-version v1.9.4
 
-    MINIKUBE_OK="false"
+    MINIKUBE_OK=false
     echo "Waiting for minikube to start..."
     # this for loop waits until kubectl can access the api server that Minikube has created
     for i in {1..90}; do # timeout for 3 minutes
-       kubectl get po &> /dev/null
-       if [ $? -ne 1 ]; then
-          MINIKUBE_OK="true"
-          break
-       fi
-       sleep 2
+        if [[ ( $(kubectl get nodes) ) && ( -z "$(kubectl get nodes | grep " Ready ")" ) ]]; then
+            MINIKUBE_OK=true
+            break
+        fi
+        sleep 2
     done
 
     # Shut down CI if minikube did not start and show logs
-    if [ $MINIKUBE_OK == "false" ]; then
-        echo "  minikube did not start"
-        sudo minikube logs
-        die $LINENO "minikube did not start"
-    else
-        echo "  minikube start successfully"
+    if [ MINIKUBE_OK ]; then
+        echo "  minikube start successfully: $cluster_name"
         minikube addons enable ingress -p $cluster_name
         kubectl label nodes $cluster_name host=development --overwrite
         kubectl create namespace development
+    else
+        echo "  minikube did not start: $cluster_name"
+        sudo minikube logs
+        die $LINENO "minikube did not start"
     fi
 }
 
