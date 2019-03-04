f9e32afd1cff34997a7722bc0a7b76b607333774
Hattori Keigo <keigoht@gmail.com>
2019-03-04 10:34:47 +0900

    Update test

--- test/apis/test_api_evaluation.py
+++ test/apis/test_api_evaluation.py
@@ -3,9 +3,13 @@ import json
 from copy import deepcopy
 
 from rekcurd_dashboard.protobuf import rekcurd_pb2
-from test.base import BaseTestCase, create_app_obj, create_service_obj, create_eval_obj, create_eval_result_obj
+from test.base import (
+    BaseTestCase, create_application_model, create_service_model, create_eval_model, create_eval_result_model,
+    TEST_PROJECT_ID, TEST_APPLICATION_ID, TEST_MODEL_ID,
+    TEST_SERVICE_ID, TEST_USER_ID_1, TEST_USER_ID_2, TEST_AUTH_ID_1, TEST_AUTH_ID_2
+)
 from io import BytesIO
-from rekcurd_dashboard.models import EvaluationResult, Evaluation, db
+from rekcurd_dashboard.models import EvaluationResultModel, EvaluationModel, db
 
 default_metrics = {'accuracy': 0.0, 'fvalue': [0.0], 'num': 0,
                    'option': {}, 'precision': [0.0], 'recall': [0.0], 'label': ['label']}
@@ -49,9 +53,7 @@ class ApiEvaluationTest(BaseTestCase):
 
     @patch_stub
     def test_post(self):
-        aobj = create_app_obj()
-
-        url = f'/api/applications/{aobj.application_id}/evaluations'
+        url = f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluations'
         content_type = 'multipart/form-data'
         file_content = b'my file contents'
         response = self.client.post(url,
@@ -68,17 +70,18 @@ class ApiEvaluationTest(BaseTestCase):
         self.assertEqual(response.json, {'status': True, 'evaluation_id': 1})
 
     def test_delete(self):
-        app_id = create_app_obj().application_id
-        eobj = create_eval_obj(app_id, save=True)
-        sobj = create_service_obj(app_id)
-        create_eval_result_obj(model_id=sobj.model_id, evaluation_id=eobj.evaluation_id, save=True)
-        response = self.client.delete(f'/api/applications/{app_id}/evaluations/{eobj.evaluation_id}')
+        evaluation_model = create_eval_model(TEST_APPLICATION_ID, save=True)
+        create_eval_result_model(model_id=TEST_MODEL_ID, evaluation_id=evaluation_model.evaluation_id, save=True)
+        response = self.client.delete(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/'
+            f'evaluations/{evaluation_model.evaluation_id}')
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json, {'status': True, 'message': 'Success.'})
-        self.assertEqual(Evaluation.query.all(), [])
-        self.assertEqual(EvaluationResult.query.all(), [])
+        self.assertEqual(EvaluationModel.query.all(), [])
+        self.assertEqual(EvaluationResultModel.query.all(), [])
 
-        response = self.client.delete(f'/api/applications/{app_id}/evaluations/101')
+        response = self.client.delete(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluations/101')
         self.assertEqual(404, response.status_code)
         self.assertEqual(response.json, {'status': False, 'message': 'Not Found.'})
 
@@ -88,44 +91,52 @@ class ApiEvaluationResultTest(BaseTestCase):
     """
     @patch_stub
     def test_get(self):
-        app_id = create_app_obj().application_id
-        eobj = create_eval_obj(app_id, save=True)
-        sobj = create_service_obj(app_id)
-        robj = create_eval_result_obj(model_id=sobj.model_id, evaluation_id=eobj.evaluation_id, save=True)
-        response = self.client.get(f'/api/applications/{app_id}/evaluation_results/{robj.evaluation_result_id}')
+        evaluation_model = create_eval_model(TEST_APPLICATION_ID, save=True)
+        eval_result_model = create_eval_result_model(
+            model_id=TEST_MODEL_ID, evaluation_id=evaluation_model.evaluation_id, save=True)
+        response = self.client.get(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/'
+            f'evaluation_results/{eval_result_model.evaluation_result_id}')
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json['status'], True)
         self.assertEqual(response.json['metrics'], default_metrics)
         details = response.json['details']
         self.assertEqual(len(details), 4)
-        self.assertEqual(details[0], {'input': 'input', 'label': 'test', 'output': 'test', 'score': 1.0, 'is_correct': True})
-        self.assertEqual(details[1], {'input': 0.5, 'label': [0.9, 1.3], 'output': [0.9, 0.3], 'score': [0.5, 0.5], 'is_correct': False})
+        self.assertEqual(
+            details[0], {'input': 'input', 'label': 'test', 'output': 'test', 'score': 1.0, 'is_correct': True})
+        self.assertEqual(
+            details[1],
+            {'input': 0.5, 'label': [0.9, 1.3], 'output': [0.9, 0.3], 'score': [0.5, 0.5], 'is_correct': False})
 
     @patch('rekcurd_dashboard.core.rekcurd_dashboard_client.rekcurd_pb2_grpc.RekcurdDashboardStub')
     def test_get_not_found(self, mock_stub_class):
-        app_id = create_app_obj().application_id
-        eobj = create_eval_obj(app_id, save=True)
-        sobj = create_service_obj(app_id)
-        robj = create_eval_result_obj(model_id=sobj.model_id, evaluation_id=eobj.evaluation_id, save=True)
-        response = self.client.get(f'/api/applications/{app_id}/evaluation_results/{robj.evaluation_result_id}')
+        evaluation_model = create_eval_model(TEST_APPLICATION_ID, save=True)
+        eval_result_model = create_eval_result_model(
+            model_id=TEST_MODEL_ID, evaluation_id=evaluation_model.evaluation_id, save=True)
+        response = self.client.get(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/'
+            f'evaluation_results/{eval_result_model.evaluation_result_id}')
         self.assertEqual(404, response.status_code)
         self.assertEqual(response.json, {'status': False, 'message': 'Result Not Found.'})
 
-        response = self.client.get(f'/api/applications/{app_id}/evaluation_results/101')
+        response = self.client.get(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluation_results/101')
         self.assertEqual(404, response.status_code)
         self.assertEqual(response.json, {'status': False, 'message': 'Not Found.'})
 
     def test_delete(self):
-        app_id = create_app_obj().application_id
-        eobj = create_eval_obj(app_id, save=True)
-        sobj = create_service_obj(app_id)
-        robj = create_eval_result_obj(model_id=sobj.model_id, evaluation_id=eobj.evaluation_id, save=True)
-        response = self.client.delete(f'/api/applications/{app_id}/evaluation_results/{robj.evaluation_result_id}')
+        evaluation_model = create_eval_model(TEST_APPLICATION_ID, save=True)
+        eval_result_model = create_eval_result_model(
+            model_id=TEST_MODEL_ID, evaluation_id=evaluation_model.evaluation_id, save=True)
+        response = self.client.delete(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/'
+            f'evaluation_results/{eval_result_model.evaluation_result_id}')
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json, {'status': True, 'message': 'Success.'})
-        self.assertEqual(EvaluationResult.query.all(), [])
+        self.assertEqual(EvaluationResultModel.query.all(), [])
 
-        response = self.client.delete(f'/api/applications/{app_id}/evaluation_results/101')
+        response = self.client.delete(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluation_results/101')
         self.assertEqual(404, response.status_code)
         self.assertEqual(response.json, {'status': False, 'message': 'Not Found.'})
 
@@ -137,53 +148,48 @@ class ApiEvaluateTest(BaseTestCase):
 
     @patch_stub
     def test_post(self):
-        aobj = create_app_obj()
-        model_id = create_service_obj(aobj.application_id).model_id
-        evaluation_id = create_eval_obj(aobj.application_id, save=True).evaluation_id
+        evaluation_id = create_eval_model(TEST_APPLICATION_ID, save=True).evaluation_id
 
         # create another service to confirm that the API works with multiple candidates
-        create_service_obj(aobj.application_id, model_id=model_id, service_name='foo', save=True)
+        create_service_model(application_id=TEST_APPLICATION_ID, model_id=TEST_MODEL_ID,
+                             service_id="new-service", display_name="new-service", save=True)
 
-        response = self.client.post(f'/api/applications/{aobj.application_id}/evaluate',
-                                    data={'evaluation_id': evaluation_id, 'model_id': model_id})
+        response = self.client.post(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluate',
+            data={'evaluation_id': evaluation_id, 'model_id': TEST_MODEL_ID})
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json, self.default_response)
-        eobj_exists = db.session.query(EvaluationResult)\
-            .filter(EvaluationResult.model_id == model_id,
-                    EvaluationResult.evaluation_id == evaluation_id).one_or_none() is not None
-        self.assertEqual(eobj_exists, True)
+        evaluation_model_exists = db.session.query(EvaluationResultModel).filter(
+            EvaluationResultModel.model_id == TEST_MODEL_ID,
+            EvaluationResultModel.evaluation_id == evaluation_id).one_or_none() is not None
+        self.assertEqual(evaluation_model_exists, True)
 
     @patch_stub
     def test_post_without_param(self):
-        aobj = create_app_obj()
-        model_id = create_service_obj(aobj.application_id).model_id
-        create_eval_obj(aobj.application_id, checksum='12345', save=True)
-        create_eval_obj(aobj.application_id, checksum='6789', save=True)
-        create_eval_obj(aobj.application_id, checksum='abc', save=True)
-        newest_eval_id = create_eval_obj(aobj.application_id, save=True).evaluation_id
+        create_eval_model(TEST_APPLICATION_ID, checksum='12345', save=True)
+        create_eval_model(TEST_APPLICATION_ID, checksum='6789', save=True)
+        create_eval_model(TEST_APPLICATION_ID, checksum='abc', save=True)
+        newest_eval_id = create_eval_model(TEST_APPLICATION_ID, save=True).evaluation_id
 
-        response = self.client.post(f'/api/applications/{aobj.application_id}/evaluate',
-                                    data={'model_id': model_id})
+        response = self.client.post(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluate',
+            data={'model_id': TEST_MODEL_ID})
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json, self.default_response)
-        eobj_exists = db.session.query(EvaluationResult)\
-            .filter(EvaluationResult.model_id == model_id,
-                    EvaluationResult.evaluation_id == newest_eval_id).one_or_none() is not None
-        self.assertEqual(eobj_exists, True)
+        evaluation_model_exists = db.session.query(EvaluationResultModel).filter(
+            EvaluationResultModel.model_id == TEST_MODEL_ID,
+            EvaluationResultModel.evaluation_id == newest_eval_id).one_or_none() is not None
+        self.assertEqual(evaluation_model_exists, True)
 
     @patch_stub
     def test_post_duplicated(self):
         saved_response = deepcopy(self.default_response)
-        aobj = create_app_obj()
-        model_id = create_service_obj(aobj.application_id).model_id
-        evaluation_id = create_eval_obj(aobj.application_id, save=True).evaluation_id
-        create_eval_result_obj(model_id=model_id,
-                               evaluation_id=evaluation_id,
-                               result=json.dumps(saved_response),
-                               save=True)
+        evaluation_id = create_eval_model(TEST_APPLICATION_ID, save=True).evaluation_id
+        create_eval_result_model(
+            model_id=TEST_MODEL_ID, evaluation_id=evaluation_id, result=json.dumps(saved_response), save=True)
 
-        url = f'/api/applications/{aobj.application_id}/evaluate'
-        data = {'evaluation_id': evaluation_id, 'model_id': model_id}
+        url = f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluate'
+        data = {'evaluation_id': evaluation_id, 'model_id': TEST_MODEL_ID}
         response = self.client.post(url, data=data)
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json, saved_response)
@@ -193,27 +199,16 @@ class ApiEvaluateTest(BaseTestCase):
         self.assertEqual(200, response.status_code)
         self.assertEqual(response.json, self.default_response)
 
-        eobj = db.session.query(EvaluationResult)\
-            .filter(EvaluationResult.model_id == model_id,
-                    EvaluationResult.evaluation_id == evaluation_id).one()
-        self.assertEqual(eobj.result, self.default_response)
+        evaluation_model = db.session.query(EvaluationResultModel).filter(
+            EvaluationResultModel.model_id == TEST_MODEL_ID,
+            EvaluationResultModel.evaluation_id == evaluation_id).one()
+        self.assertEqual(evaluation_model.result, self.default_response)
 
     @patch_stub
     def test_post_not_found(self):
-        application_id = create_app_obj().application_id
-        model_id = create_service_obj(application_id).model_id
-        new_sobj = create_service_obj(application_id,
-                                      service_name='rekcurd-test-app-production-20180628151929',
-                                      service_level='production',
-                                      model_id=model_id + 1,
-                                      save=True)
-        evaluation_id = create_eval_obj(application_id, save=True).evaluation_id
-
-        response = self.client.post(f'/api/applications/{application_id}/evaluate',
-                                    data={'evaluation_id': evaluation_id, 'model_id': new_sobj.model_id})
-        self.assertEqual(404, response.status_code)
-
+        evaluation_id = create_eval_model(TEST_APPLICATION_ID, save=True).evaluation_id
         non_exist_model_id = 100
-        response = self.client.post(f'/api/applications/{application_id}/evaluate',
-                                    data={'evaluation_id': evaluation_id, 'model_id': non_exist_model_id})
+        response = self.client.post(
+            f'/api/projects/{TEST_PROJECT_ID}/applications/{TEST_APPLICATION_ID}/evaluate',
+            data={'evaluation_id': evaluation_id, 'model_id': non_exist_model_id})
         self.assertEqual(404, response.status_code)
--- test/base.py
+++ test/base.py
@@ -2,115 +2,173 @@ import warnings
 
 from flask_testing import TestCase
 
-from rekcurd_dashboard.models import db, Application, Service, Evaluation, EvaluationResult, User, ApplicationUserRole, Role
+from rekcurd_dashboard.models import (
+    db, ApplicationModel, ServiceModel, EvaluationModel, ProjectModel, ModelModel,
+    EvaluationResultModel, UserModel, ApplicationUserRoleModel, ApplicationRole,
+    ProjectUserRoleModel, ProjectRole, DataServerModel, DataServerModeEnum, KubernetesModel
+)
 from rekcurd_dashboard.core import create_app
+
+
+TEST_PROJECT_ID = 1
+TEST_APPLICATION_ID = "test-application"
+TEST_MODEL_ID = 1
+TEST_SERVICE_ID = "test-service"
+TEST_AUTH_ID_1 = "test-user-1"
+TEST_AUTH_ID_2 = "test-user-2"
+TEST_USER_ID_1 = 1
+TEST_USER_ID_2 = 2
 
 
 class BaseTestCase(TestCase):
     def create_app(self):
-        return create_app("test/test-settings.yml")
+        app, _ = create_app("test/test-settings.yml")
+        return app
 
     @classmethod
     def setUpClass(cls):
-        warnings.filterwarnings("ignore",
-                                category=ImportWarning,
-                                message="can't resolve package from __spec__ or __package__, falling back on __name__ and __path__")
+        warnings.filterwarnings(
+            "ignore", category=ImportWarning,
+            message="can't resolve package from __spec__ or __package__, "
+                    "falling back on __name__ and __path__")
 
     def setUp(self):
         db.create_all()
-        aobj = create_app_obj(save=True)
-        create_service_obj(aobj.application_id, save=True)
+        create_project_model(save=True)
+        create_application_model(save=True)
+        create_model_model(save=True)
+        create_service_model(save=True)
+        create_user_model(user_id=TEST_USER_ID_1, auth_id=TEST_AUTH_ID_1, user_name='TEST USER', save=True)
+        create_user_model(user_id=TEST_USER_ID_2, auth_id=TEST_AUTH_ID_2, user_name='OTHER USER', save=True)
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
 
-def create_app_obj(kubernetes_id=1, save=False):
-    app_name = 'rekcurd-test-app'
-    aobj = Application(application_name=app_name, kubernetes_id=kubernetes_id)
-    aobj_ = Application.query.filter_by(
-        application_name=app_name,
-        kubernetes_id=kubernetes_id).one_or_none()
-    if save and aobj_ is None:
-        db.session.add(aobj)
+def create_project_model(project_id=TEST_PROJECT_ID, save=False) -> ProjectModel:
+    display_name = "test-project"
+    project_model = ProjectModel(
+        project_id=project_id, display_name=display_name)
+    project_model_ = ProjectModel.query.filter_by(project_id=project_id).one_or_none()
+    if save and project_model_ is None:
+        db.session.add(project_model)
         db.session.commit()
-        return aobj
+        return project_model
     else:
-        return aobj_
+        return project_model_
 
 
-def create_service_obj(
-        application_id,
-        model_id=3,
-        service_name='rekcurd-test-app-development-20180628151929',
-        service_level='development',
-        host='localhost:5000',
-        save=False):
-    sobj = Service(application_id=application_id,
-                   service_name=service_name,
-                   service_level=service_level,
-                   host=host,
-                   model_id=model_id,
-                   display_name=service_name)
-    sobj_ = Service.query.filter_by(
-        service_name=service_name).one_or_none()
-    if save and sobj_ is None:
-        db.session.add(sobj)
+def create_application_model(
+        project_id=TEST_PROJECT_ID, application_id=TEST_APPLICATION_ID, save=False) -> ApplicationModel:
+    application_name = 'test-application'
+    application_model = ApplicationModel(
+        application_id=application_id, application_name=application_name,
+        project_id=project_id)
+    application_model_ = ApplicationModel.query.filter_by(
+        application_name=application_name,
+        project_id=project_id).one_or_none()
+    if save and application_model_ is None:
+        db.session.add(application_model)
         db.session.commit()
-        return sobj
+        return application_model
     else:
-        return sobj_
+        return application_model_
 
 
-def create_eval_obj(
-        application_id,
-        checksum='abcde',
-        data_path='my_data_path',
-        save=False):
-    eobj = Evaluation(checksum=checksum,
-                      application_id=application_id,
-                      data_path=data_path)
+def create_model_model(
+        application_id=TEST_APPLICATION_ID, model_id=TEST_MODEL_ID, file_path="rekcurd-test-model/test.model",
+        description="rekcurd-test-model", save=False) -> ModelModel:
+    model_model = ModelModel(
+        model_id=model_id, application_id=application_id,
+        description=description, filepath=file_path)
+    model_model_ = ModelModel.query.filter_by(model_id=model_id).one_or_none()
+    if save and model_model_ is None:
+        db.session.add(model_model)
+        db.session.commit()
+        return model_model
+    else:
+        return model_model_
+
+
+def create_service_model(
+        service_id=TEST_SERVICE_ID, application_id=TEST_APPLICATION_ID, model_id=TEST_MODEL_ID,
+        display_name='test-service', service_level='development',
+        version='v2', host='localhost', port=5000, save=False) -> ServiceModel:
+    service_model = ServiceModel(
+        service_id=service_id, application_id=application_id,
+        display_name=display_name, service_level=service_level,
+        version=version, model_id=model_id,
+        host=host, port=port)
+    service_model_ = ServiceModel.query.filter_by(
+        application_id=application_id, display_name=display_name).one_or_none()
+    if save and service_model_ is None:
+        db.session.add(service_model)
+        db.session.commit()
+        return service_model
+    else:
+        return service_model_
+
+
+def create_eval_model(
+        application_id=TEST_APPLICATION_ID, checksum='abcde',
+        data_path='my_data_path', save=False) -> EvaluationModel:
+    evaluation_model = EvaluationModel(
+        checksum=checksum, application_id=application_id, data_path=data_path)
     if save:
-        db.session.add(eobj)
+        db.session.add(evaluation_model)
         db.session.commit()
-    return eobj
+    return evaluation_model
 
 
-def create_eval_result_obj(
-        model_id,
-        evaluation_id,
-        data_path='my_data_path',
-        result='{}',
-        save=False):
-    robj = EvaluationResult(model_id=model_id,
-                            data_path='my_result_path',
-                            evaluation_id=evaluation_id,
-                            result=result)
+def create_eval_result_model(
+        evaluation_id, model_id=TEST_MODEL_ID,
+        data_path='my_data_path', result='{}', save=False) -> EvaluationResultModel:
+    evaluation_result_model = EvaluationResultModel(
+        model_id=model_id, data_path=data_path,
+        evaluation_id=evaluation_id, result=result)
     if save:
-        db.session.add(robj)
+        db.session.add(evaluation_result_model)
         db.session.commit()
-    return robj
+    return evaluation_result_model
 
 
-def create_user_obj(auth_id, user_name, save=False):
-    uobj = User(auth_id=auth_id, user_name=user_name)
-    uobj_ = User.query.filter_by(auth_id=auth_id).one_or_none()
-    if save and uobj_ is None:
-        db.session.add(uobj)
+def create_user_model(user_id, auth_id, user_name, save=False) -> UserModel:
+    user_model = UserModel(user_id=user_id, auth_id=auth_id, user_name=user_name)
+    user_model_ = UserModel.query.filter_by(auth_id=auth_id).one_or_none()
+    if save and user_model_ is None:
+        db.session.add(user_model)
         db.session.commit()
-        return uobj
+        return user_model
     else:
-        return uobj_
+        return user_model_
 
 
-def create_application_user_role_obj(application_id, user_id, role=Role.viewer, save=True):
-    robj = ApplicationUserRole(application_id=application_id, user_id=user_id, role=role)
-    robj_ = ApplicationUserRole.query.filter_by(application_id=application_id, user_id=user_id).one_or_none()
-    if save and robj_ is None:
-        db.session.add(robj)
+def create_project_user_role_model(
+        project_id, user_id, project_role=ProjectRole.member, save=True) -> ProjectUserRoleModel:
+    project_user_role_model = ProjectUserRoleModel(
+        project_id=project_id, user_id=user_id, project_role=project_role)
+    project_user_role_model_ = ProjectUserRoleModel.query.filter_by(
+        project_id=project_id, user_id=user_id).one_or_none()
+    if save and project_user_role_model_ is None:
+        db.session.add(project_user_role_model)
         db.session.commit()
-        return robj
+        return project_user_role_model
     else:
-        robj_.role = role
-        return robj_
+        project_user_role_model_.project_role = project_role
+        return project_user_role_model_
+
+
+def create_application_user_role_model(
+        application_id, user_id, application_role=ApplicationRole.viewer, save=True) -> ApplicationUserRoleModel:
+    application_user_role_model = ApplicationUserRoleModel(
+        application_id=application_id, user_id=user_id, application_role=application_role)
+    application_user_role_model_ = ApplicationUserRoleModel.query.filter_by(
+        application_id=application_id, user_id=user_id).one_or_none()
+    if save and application_user_role_model_ is None:
+        db.session.add(application_user_role_model)
+        db.session.commit()
+        return application_user_role_model
+    else:
+        application_user_role_model_.application_role = application_role
+        return application_user_role_model_
