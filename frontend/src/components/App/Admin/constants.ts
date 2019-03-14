e1bcbc83703dec3f3eb267dfb2a6fab98e9dde6a
Hattori Keigo <keigoht@gmail.com>
2019-03-14 16:02:46 +0900

    Update constants

--- frontend/src/components/App/Admin/constants.ts
+++ frontend/src/components/App/Admin/constants.ts
@@ -1,17 +1,48 @@
-export enum role {
-    viewer = 'viewer',
-    editor = 'editor',
-    owner  = 'owner'
+export enum dataServerMode {
+    local = 'local',
+    ceph_s3  = 'ceph_s3',
+    aws_s3  = 'aws_s3'
 }
 
-export const apiConvert = (param) => {
-    if (param === 'Role.viewer') {
-        return role.viewer.toString()
-    } else if (param === 'Role.editor') {
-        return role.editor.toString()
-    } else if (param === 'Role.owner') {
-        return role.owner.toString()
-    } else {
-        return role.viewer.toString()
-    }
+export enum projectRole {
+    member = 'member',
+    admin  = 'admin'
 }
+
+export enum applicationRole {
+    viewer = 'viewer',
+    editor = 'editor',
+    admin  = 'admin'
+}
+
+export const apiConvertDataServerMode = (param) => {
+    if (param === 'DataServerModeEnum.local') {
+        return dataServerMode.local.toString()
+    } else if (param === 'DataServerModeEnum.ceph_s3') {
+        return dataServerMode.ceph_s3.toString()
+    } else if (param === 'DataServerModeEnum.aws_s3') {
+        return dataServerMode.aws_s3.toString()
+    } else {
+        return false
+    }
+};
+
+export const apiConvertProjectRole = (param) => {
+    if (param === 'ProjectRole.member') {
+        return projectRole.member.toString()
+    } else if (param === 'ProjectRole.admin') {
+        return projectRole.admin.toString()
+    } else {
+        return false
+    }
+};
+
+export const apiConvertApplicationRole = (param) => {
+    if (param === 'ApplicationRole.admin') {
+        return applicationRole.admin.toString()
+    } else if (param === 'ApplicationRole.editor') {
+        return applicationRole.editor.toString()
+    } else {
+        return applicationRole.viewer.toString()
+    }
+};
