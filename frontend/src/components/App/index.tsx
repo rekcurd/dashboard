d8e0c7b19492e2aa312d3336c6daa845873e6884
Hattori Keigo <keigoht@gmail.com>
2019-04-08 11:46:05 +0900

    Renew `App`

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -3,22 +3,30 @@ import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
 import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom'
 
-import Layout from './Layout'
-import Applications from './Applications'
-import SaveApplication from './SaveApplication'
-import Application from './Application'
-import Deploy from './Deploy'
-import Services from './Services'
-import Models from './Models'
-import Service from './Service'
-import Model from './Model'
-import Settings from './Settings'
-import Login from './Login'
-import Admin from './Admin'
-import * as Kubernetes from './Kubernetes'
 import { APIRequestResultsRenderer } from '@components/Common/APIRequestResultsRenderer'
 import { AppState } from '@src/reducers'
 import { settingsDispatcher } from '@src/actions'
+
+import Layout from './Layout'
+import Login from './Login'
+
+import Projects from './Projects'
+import ProjectAdmin from './ProjectAdmin'
+import DataServer from './DataServer'
+import * as Kubernetes from './Kubernetes'
+import SaveProject from './SaveProject'
+import Applications from './Applications'
+import SaveApplication from './SaveApplication'
+
+import ApplicationAdmin from './ApplicationAdmin'
+import Application from './Application'
+import Dashboard from './Dashboard'
+import Services from './Services'
+import Service from './Service'
+import Models from './Models'
+import Model from './Model'
+import ServiceRouting from './ServiceRouting'
+
 
 interface AppStateProps {
   settingsState: AppState
@@ -52,18 +60,38 @@ class AppComponent extends React.Component<AppProps> {
   renderApp(results) {
     const settings: any = results.settings
     let login
+    let projectAdmin
+    let dataServerAdmin
+    let kubenetesAdmin
+    let applicationAdmin
     if (settings.auth) {
       login = <Route path='/login' component={Login} />
+      projectAdmin = <Route exact path='/projects/:projectId/admin' component={ProjectAdmin} />
+      dataServerAdmin = <Route exact path='/projects/:projectId/data_servers' component={DataServer} />
+      kubenetesAdmin = <Route path='/projects/:projectId/kubernetes' component={KubernetesRoute} />
+      applicationAdmin = <Route exact path='/projects/:projectId/applications/:applicationId/admin' component={ApplicationAdmin} />
+    } else {
+      login = null
+      projectAdmin = <Redirect from='/projects/:projectId/admin' to='/projects/:projectId/applications' />
+      dataServerAdmin = <Redirect from='/projects/:projectId/data_servers' to='/projects/:projectId/applications' />
+      kubenetesAdmin = <Redirect from='/projects/:projectId/kubernetes' to='/projects/:projectId/applications' />
+      applicationAdmin = <Redirect from='/projects/:projectId/applications/:applicationId/admin' to='/projects/:projectId/applications/:applicationId/dashboard' />
     }
     return (
       <Layout auth={settings.auth}>
         <Switch>
-          <Redirect exact from='/' to='/applications' />
-          <Route exact path='/applications' component={Applications} />
-          <Route exact path='/applications/add' component={SaveApplication} />
-          <Route path='/applications/:applicationId' render={() => <ApplicationRoute settings={settings} />} />
-          <Route path='/settings' component={SettingsRoute} />
+          <Redirect exact from='/' to='/projects' />
+          <Redirect exact from='/projects/:projectId' to='/projects/:projectId/applications' />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/projects/add' component={SaveProject} />
+          <Route exact path='/projects/:projectId/applications' component={Applications} />
+          <Route exact path='/projects/:projectId/applications/add' component={SaveApplication} />
+          <Route path='/projects/:projectId/applications/:applicationId' render={() => <ApplicationRoute settings={settings} />} />
           {login}
+          {projectAdmin}
+          {dataServerAdmin}
+          {kubenetesAdmin}
+          {applicationAdmin}
         </Switch>
       </Layout>
     )
@@ -78,55 +106,46 @@ export const App = connect<AppStateProps, AppDispatchProps>(
   }),
 )(AppComponent)
 
+
 interface ApplicationRouteProps {
   settings: any
 }
+
 class ApplicationRoute extends React.Component<ApplicationRouteProps> {
   render() {
-    const { settings } = this.props
-    let admin
-    if (settings.auth) {
-      admin = <Route path='/applications/:applicationId/admin' component={Admin} />
-    } else {
-      admin = <Redirect from='/applications/:applicationId/admin' to='/applications/:applicationId' />
-    }
     return (
       <Application>
         <Switch>
-          <Route path='/applications/:applicationId/dashboard' component={Deploy} />
-          <Route exact path='/applications/:applicationId/services' component={Services} />
-          <Route exact path='/applications/:applicationId/models' component={Models} />
-          <Route path='/applications/:applicationId/services/add'
-            render={(props) => <Service {...props} mode='add'/>} />
-          <Route path='/applications/:applicationId/services/:serviceId/edit'
-            render={(props) => <Service {...props} mode='edit'/>} />
-          <Route path='/applications/:applicationId/models/:modelId/edit'
-                render={(props) => <Model {...props} mode='edit'/>} />
-          <Redirect exact from='/applications/:applicationId' to='/applications/:applicationId/dashboard' />
-          {admin}
+          <Route path='/projects/:projectId/applications/:applicationId/dashboard' component={Dashboard} />
+          <Route exact path='/projects/:projectId/applications/:applicationId/services' component={Services} />
+          <Route exact path='/projects/:projectId/applications/:applicationId/models' component={Models} />
+          <Route exact path='/projects/:projectId/applications/:applicationId/routing' component={ServiceRouting} />
+          <Route exact path='/projects/:projectId/applications/:applicationId/services/add'
+                 render={(props) => <Service {...props} method='post'/>} />
+          <Route exact path='/projects/:projectId/applications/:applicationId/services/:serviceId/edit'
+                 render={(props) => <Service {...props} method='patch'/>} />
+          <Route exact path='/projects/:projectId/applications/:applicationId/models/:modelId/edit'
+                 render={(props) => <Model {...props} method='patch'/>} />
+          <Redirect exact from='/projects/:projectId/applications/:applicationId' to='/projects/:projectId/applications/:applicationId/dashboard' />
         </Switch>
       </Application>
     )
   }
 }
 
-const SettingsRoute = () => (
-  <Settings>
-    <Switch>
-      <Route path='/settings/kubernetes' component={KubernetesRoute} />
-    </Switch>
-  </Settings>
-)
+class KubernetesRoute extends React.Component {
+  render() {
+    return(
+      <Kubernetes.SideMenu>
+        <Switch>
+          <Route exact path='/projects/:projectId/kubernetes' component={Kubernetes.Hosts} />
+          <Route exact path='/projects/:projectId/kubernetes/add'
+                 render={(props) => <Kubernetes.Host {...props} method='post' />} />
+          <Route exact path='/projects/:projectId/kubernetes/:kubernetesId'
+                 render={(props) => <Kubernetes.Host {...props} method='patch' />} />
+        </Switch>
+      </Kubernetes.SideMenu>
+    )
+  }
+}
 
-const KubernetesRoute = () => (
-  <Kubernetes.Kubernetes>
-    <Switch>
-      <Redirect exact from='/settings/kubernetes' to='/settings/kubernetes/hosts' />
-      <Route exact path='/settings/kubernetes/hosts' component={Kubernetes.Hosts} />
-      <Route exact path='/settings/kubernetes/hosts/add'
-        render={(props) => <Kubernetes.Host {...props} mode='add'/>} />
-      <Route exact path='/settings/kubernetes/hosts/:kubernetesId/edit'
-        render={(props) => <Kubernetes.Host {...props} mode='edit'/>} />
-    </Switch>
-  </Kubernetes.Kubernetes>
-)
