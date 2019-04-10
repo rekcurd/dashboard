417b7077e3c0b8f8984cca0bc725d2c7d808829d
Hattori Keigo <keigoht@gmail.com>
2019-04-10 10:05:56 +0900

    Add Formik Field component

---
+++ frontend/src/components/Common/Field/FormikInput.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { FormGroup, Label, Input, FormFeedback } from 'reactstrap'
+
+
+/**
+ * General single form field component with validation message
+ *
+ *
+ */
+export const FormikInput = (
+  {
+    options = [] as Array<{label: string, value: string, disabled: boolean}>,
+    required,
+    field: {...fields},
+    form: {touched, errors, setFieldValue, ...rest},
+    ...props
+  }) => {
+  const margin = 'mb-3'
+  const renderOptionElements = () => ([{label: '', value: '', disabled: false}].concat(options)).map((v) => {
+    return (
+      <option value={v.value} key={v.label} disabled={v.disabled}>
+        {v.label}
+      </option>
+    )
+  })
+  const requiredClass = required ? 'required' : ''
+
+  return (
+    <FormGroup className={`${margin}`}>
+      <Label for={props.id} className={`${requiredClass} text-info`}>{props.label}</Label>
+      <Input
+        {...props}
+        {...fields}
+        onChange={(event) => {setFieldValue(fields.name, event.target.value); props.onChange ? props.onChange(event) : null;}}
+        invalid={Boolean(touched[fields.name] && errors[fields.name])} >
+        {options.length > 0 ? renderOptionElements() : null}
+      </Input>
+      {touched[fields.name] && errors[fields.name] ? <FormFeedback>{errors[fields.name]}</FormFeedback> : ''}
+    </FormGroup>
+  )
+}
--- frontend/src/components/Common/Field/index.tsx
+++ frontend/src/components/Common/Field/index.tsx
@@ -1,2 +1,4 @@
 export { Checkbox } from './Checkbox'
 export { Radio } from './Radio'
+export { FormikInput } from './FormikInput'
+export { FileUpload } from './FileUpload'
