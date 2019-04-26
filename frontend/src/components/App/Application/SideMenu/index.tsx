7c580a7dff6819bc72f930b140483f59611c4309
Hattori Keigo <keigoht@gmail.com>
2019-04-26 11:06:55 +0900

    Use Dropdown list

--- frontend/src/components/App/Application/SideMenu/index.tsx
+++ frontend/src/components/App/Application/SideMenu/index.tsx
@@ -11,6 +11,13 @@ import { serviceLevel } from '@components/Common/Enum'
 class SideMenu extends React.Component<SideMenuProps, SideMenuState> {
   constructor(props, context) {
     super(props, context)
+    this.state = {
+      gitKeyDropdownOpen: false,
+      routingDropdownOpen: false
+    }
+
+    this.gitKeyToggle = this.gitKeyToggle.bind(this)
+    this.routingToggle = this.routingToggle.bind(this)
   }
 
   componentDidMount() {
@@ -27,117 +34,114 @@ class SideMenu extends React.Component<SideMenuProps, SideMenuState> {
     const { fetchProjectByIdStatus, application, userInfoStatus } = this.props
     const { projectId, applicationId } = this.props.match.params
 
-    const menuContents = [
-      {
-        title: 'Application',
-        items: [
-          {
-            text: 'Dashboard',
-            path: 'dashboard',
-            icon: 'chart-line',
-            items: []
-          },
-          {
-            text: 'Services',
-            path: 'services',
-            icon: 'server',
-            items: []
-          },
-          {
-            text: 'Models',
-            path: 'models',
-            icon: 'database',
-            items: []
-          },
-        ]
-      }
-    ]
+    const mainContents = {
+      title: 'Application',
+      items: [
+        {
+          text: 'Dashboard',
+          path: 'dashboard',
+          icon: 'chart-line',
+        },
+        {
+          text: 'Services',
+          path: 'services',
+          icon: 'server',
+        },
+        {
+          text: 'Models',
+          path: 'models',
+          icon: 'database',
+        },
+      ]
+    }
+
+    let kubernetesContents: React.ReactNode
     if (fetchProjectByIdStatus.status === APIRequestStatusList.success && fetchProjectByIdStatus.result.useKubernetes) {
       const serviceLevels = (parentPath: string) => {
         const contents = Object.values(serviceLevel).map((serviceLevelName: string) => {
-          return {
-            text: serviceLevelName,
-            path: `${parentPath}/${serviceLevelName}`
-          }
+          const path = `${parentPath}/${serviceLevelName}`
+          return (
+            <li className='navbar-item mb-1' key={path}>
+              <NavLink exact className='nav-link text-info pt-1 pb-1' to={this.fullPath(path)}>
+                <span className="fa-fw pl-4">{serviceLevelName}</span>
+              </NavLink>
+            </li>
+          )
         })
         return contents
       }
-      menuContents.push({
-        title: 'Kubernetes',
-        items: []
-      })
-      menuContents[1]['items'].push({
-        text: 'Git Key',
-        path: 'git_key',
-        icon: 'key',
-        items: serviceLevels('git_key')
-      })
-      menuContents[1]['items'].push({
-        text: 'Routing',
-        path: 'routing',
-        icon: 'route',
-        items: serviceLevels('routing')
-      })
+
+      const gitKeyPath = 'git_key'
+      const gitKeyDropdown = (
+        <li className='navbar-item mb-1' key={gitKeyPath}>
+          <span className='nav-link text-info' onClick={this.gitKeyToggle}>
+            <i className="fas fa-key fa-fw mr-2"></i>
+            Git Key
+            <i className="fa fa-caret-down fa-fw ml-1"></i>
+          </span>
+        </li>
+      )
+      const gitKeyContents = serviceLevels(gitKeyPath)
+      const routingPath = 'routing'
+      const routingDropdown = (
+        <li className='navbar-item mb-1' key={routingPath}>
+          <span className='nav-link text-info' onClick={this.routingToggle}>
+            <i className="fas fa-route fa-fw mr-2"></i>
+            Routing
+            <i className="fa fa-caret-down fa-fw ml-1"></i>
+          </span>
+        </li>
+      )
+      const routingContents = serviceLevels(routingPath)
+
+      const title = 'Kubernetes'
+      kubernetesContents = (
+        <li className='navbar-item' key={title}>
+          { /* SubMenu */ }
+          <h4 className='navhead mb-2 pb-1 border-bottom border-secondary'>{title}</h4>
+          <ul className='nav flex-column mb-4'>
+            {gitKeyDropdown}
+            {this.state.gitKeyDropdownOpen ? gitKeyContents : null}
+            {routingDropdown}
+            {this.state.routingDropdownOpen ? routingContents : null}
+          </ul>
+        </li>
+      )
     }
+
+    let adminContents = null
     if (userInfoStatus.status === APIRequestStatusList.success) {
-      menuContents.push({
+      adminContents = {
         title: 'Access Control',
         items: [
           {
             text: 'Admin',
             path: 'admin',
             icon: 'user-lock',
-            items: []
           }
         ]
-      })
+      }
     }
-
-    const fullPath = (menuPath) => `/projects/${projectId}/applications/${applicationId}/${menuPath}`
 
     const renderSubmenuItem = (subMenuItem) => {
-      let children = null
-      if (subMenuItem.items) {
-        const child = subMenuItem.items.map((subsubMenuItem) => {
-          return (
-            <li key={subsubMenuItem.text}>
-              <NavLink exact className='nav-link text-info' to={fullPath(subsubMenuItem.path)}>
-                {subsubMenuItem.text}
-              </NavLink>
-            </li>
-          )
-        })
-        children = (
-          <li key={subMenuItem.path + '-subsubmenu'}>
-            <ul style={{listStyleType: "none"}}>
-              {child}
-            </ul>
-          </li>
-        )
-      }
       return (
-        <React.Fragment key={subMenuItem.path}>
-          <li className='navbar-item mb-1' key={subMenuItem.path}>
-            <NavLink exact className='nav-link text-info' to={fullPath(subMenuItem.path)}>
-              <i className={`fas fa-${subMenuItem.icon} fa-fw mr-2`}></i>
-              {subMenuItem.text}
-            </NavLink>
-          </li>
-          {children}
-        </React.Fragment>
+        <li className='navbar-item mb-1' key={subMenuItem.path}>
+          <NavLink exact className='nav-link text-info' to={this.fullPath(subMenuItem.path)}>
+            <i className={`fas fa-${subMenuItem.icon} fa-fw mr-2`}></i>
+            {subMenuItem.text}
+          </NavLink>
+        </li>
       )
     }
 
-    const generatedMenu = menuContents.map(
-      (subMenu) => (
-        <li className='navbar-item' key={subMenu.title}>
-          { /* SubMenu */ }
-          <h4 className='navhead mb-2 pb-1 border-bottom border-secondary'>{subMenu.title}</h4>
-          <ul className='nav flex-column mb-4'>
-            { subMenu.items.map((subMenuItem) => renderSubmenuItem(subMenuItem)) }
-          </ul>
-        </li>
-      )
+    const generatedMenu = (subMenu) => (
+      <li className='navbar-item' key={subMenu.title}>
+        { /* SubMenu */ }
+        <h4 className='navhead mb-2 pb-1 border-bottom border-secondary'>{subMenu.title}</h4>
+        <ul className='nav flex-column mb-4'>
+          { subMenu.items.map((subMenuItem) => renderSubmenuItem(subMenuItem)) }
+        </ul>
+      </li>
     )
 
     return (
@@ -145,17 +149,37 @@ class SideMenu extends React.Component<SideMenuProps, SideMenuState> {
         <div className='pl-3 pr-3'>
           { /* Whole Menu*/ }
           <ul className='nav flex-column navbar-light'>
-            {generatedMenu}
+            {generatedMenu(mainContents)}
+            {kubernetesContents ? kubernetesContents : null}
+            {adminContents ? generatedMenu(adminContents) : null}
           </ul>
         </div>
       </nav>
     )
   }
+
+  gitKeyToggle() {
+    this.setState({
+      gitKeyDropdownOpen: !this.state.gitKeyDropdownOpen
+    });
+  }
+  routingToggle() {
+    this.setState({
+      routingDropdownOpen: !this.state.routingDropdownOpen
+    });
+  }
+  private fullPath(menuPath: string) {
+    const {projectId, applicationId} = this.props.match.params
+    return `/projects/${projectId}/applications/${applicationId}/${menuPath}`
+  }
 }
 
 type SideMenuProps = DispatchProps & StateProps & RouteComponentProps<{projectId: number, applicationId: string}>
 
-interface SideMenuState {}
+interface SideMenuState {
+  gitKeyDropdownOpen
+  routingDropdownOpen
+}
 
 interface StateProps {
   fetchProjectByIdStatus: APIRequest<Project>
